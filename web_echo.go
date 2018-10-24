c9bc7d83c9d5a3b3db0b60dc32737fe862813a6a
liaolinrong <liaolinrong@126.com>
2018-10-24 16:51:25 +0800

    add /hostname route

--- web_echo.go
+++ web_echo.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net"
 	"net/http"
+	"os"
 )
 
 func echoHandler(w http.ResponseWriter, r *http.Request) {
@@ -30,11 +31,40 @@ func echoHandler(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, ipString)
 }
 
+func echoHostnameHandler(w http.ResponseWriter, r *http.Request) {
+	addrs, err := net.InterfaceAddrs()
+
+	if err != nil {
+		log.Fatal("InterfaceAddrs: ", err.Error())
+	}
+
+	ipString := "This is "
+	for _, address := range addrs {
+
+		// 检查ip地址判断是否回环地址
+		if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				ipString += ipnet.IP.String() + " "
+			}
+
+		}
+	}
+
+	hostname, err := os.Hostname()
+	if err != nil {
+		ipString += ", failed to get hostname \r\n"
+	} else {
+		ipString += ", hostname is " + hostname + " \r\n"
+	}
+
+	io.WriteString(w, ipString)
+}
+
 func main() {
 	http.HandleFunc("/", echoHandler)
+	http.HandleFunc("/hostname", echoHostnameHandler)
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err.Error())
 	}
 }
-
